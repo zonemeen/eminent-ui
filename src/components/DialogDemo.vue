73a4402e00f4453b4d72a3f0ea1ae2898576cca5
miqilin21 <994718917@qq.com>
2020-12-13 17:49:21 +0800

    openDialog 一句话打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,58 +1,64 @@
 <template>
-  <div>Dialog 示例</div>
-  <h1>示例1</h1>
-  <div style="position: relative; z-index: 1">
-    <Button @click="toggle">toggle</Button>
-    <Dialog
-      v-model:visible="x"
-      :closeOnClickOverlay="false"
-      :ok="f1"
-      :cancel="f2"
-    >
-      <template v-slot:content>
-        <strong>hi</strong>
-        <div>hi2</div>
-      </template>
-      <template v-slot:title>
-        <strong>加粗的标题</strong>
-      </template>
-    </Dialog>
-  </div>
-  <div
-    style="
-      position: relative;
-      z-index: 2;
-      width: 300px;
-      height: 300px;
-      background: red;
-    "
-  ></div>
+<div>Dialog 示例</div>
+<h1>示例1</h1>
+<Button @click="toggle">toggle</Button>
+<Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+  <template v-slot:content>
+    <strong>hi</strong>
+    <div>hi2</div>
+  </template>
+  <template v-slot:title>
+    <strong>加粗的标题</strong>
+  </template>
+</Dialog>
+<h1>示例2</h1>
+<Button @click="showDialog">show</Button>
+</div>
 </template>
 
 <script lang="ts">
-import Dialog from "../lib/Dialog.vue";
-import Button from "../lib/Button.vue";
-import { ref } from "vue";
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {
+  ref,
+  h
+} from 'vue'
+import {
+  openDialog
+} from '../lib/openDialog'
 export default {
   components: {
     Dialog,
-    Button,
+    Button
   },
   setup() {
-    const x = ref(false);
+    const x = ref(false)
     const toggle = () => {
-      x.value = !x.value;
-    };
+      x.value = !x.value
+    }
     const f1 = () => {
-      return false;
-    };
-    const f2 = () => {};
+      return false
+    }
+    const f2 = () => {}
+    const showDialog = () => {
+      openDialog({
+        title: h('strong', {}, '标题'),
+        content: '你好',
+        ok() {
+          console.log('ok')
+        },
+        cancel() {
+          console.log('cancel')
+        }
+      })
+    }
     return {
       x,
       toggle,
       f1,
       f2,
-    };
-  },
-};
+      showDialog
+    }
+  }
+}
 </script>
