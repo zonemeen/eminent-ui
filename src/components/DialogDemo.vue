926916e5c89db28e4a4aa0c2b0a333616bf95d01
miqilin21 <994718917@qq.com>
2020-12-13 16:07:07 +0800

    Dialog 支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,12 @@
   <div>Dialog 示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x"></Dialog>
+  <Dialog
+    v-model:visible="x"
+    :closeOnClickOverlay="false"
+    :ok="f1"
+    :cancel="f2"
+  ></Dialog>
 </template>
 
 <script lang="ts">
@@ -19,9 +24,15 @@ export default {
     const toggle = () => {
       x.value = !x.value;
     };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
     return {
       x,
       toggle,
+      f1,
+      f2,
     };
   },
 };
