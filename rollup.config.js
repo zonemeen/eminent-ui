dae99dff752c0bf4c049d43efa23c87868e91e76
miqilin21 <994718917@qq.com>
2020-12-16 18:15:00 +0800

    支持ESM导出

--- rollup.config.js
+++ rollup.config.js
@@ -6,15 +6,23 @@ import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/lib/index.ts",
-  output: {
-    globals: {
-      vue: "Vue",
+  output: [
+    {
+      globals: {
+        vue: "Vue",
+      },
+      name: "Gulu",
+      file: "dist/lib/gulu.js",
+      format: "umd",
+      plugins: [terser()],
     },
-    name: "Gulu",
-    file: "dist/lib/gulu.js",
-    format: "umd",
-    plugins: [terser()],
-  },
+    {
+      name: "Gulu",
+      file: "dist/lib/gulu.esm.js",
+      format: "es",
+      plugins: [terser()],
+    },
+  ],
   plugins: [
     scss({ include: /\.scss$/, sass: dartSass }),
     esbuild({
